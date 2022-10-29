00fff747e362c868bb9225a72889ac132609ce58
silviawolves <silviacanepa.w@gmail.com>
2022-10-29 11:03:33 +0200

    add datelocation component

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import 'antd/dist/antd.css';
 import './App.css';
 import SearchBar from './components/SearchBar';
 import './App.css';
+import DateLocation from './components/DateLocation';
 
 function App() {
     return (
         <div className="App">
             <SearchBar />
+            <DateLocation />
         </div>
     );
 }
---
+++ src/components/DateLocation.jsx
@@ -0,0 +1,17 @@
+import '../assets/css/datelocation.css';
+
+// getCity = (searchValue) => {
+//     this.setState({message: searchValue})
+// },
+
+function DateLocation() {
+    return (
+        <div className="dl-wrapper">
+            {/* <h1>{searchValue}</h1> */}
+            <h1>Città</h1>
+            <p className="date-time">00.00 00/00/0000</p>
+        </div>
+    );
+}
+
+export default DateLocation;
