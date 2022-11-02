abcdd6738252d047b815a7dd0dd7d29bcb771014
silviawolves <silviacanepa.w@gmail.com>
2022-11-02 19:23:15 +0100

    fix spacing

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -3,25 +3,27 @@ import '../assets/css/weather.css';
 
 function Weather(props) {
     return (
-        <Row align="middle" justify="center" style={{textAlign: 'center'}}>
-            <Col span={6}>
-                <p style={{textTransform: 'capitalize'}}>
-                    {props.data.weather[0].description}
-                </p>
-            </Col>
+        <div className="weather-wrapper">
+            <Row align="middle" justify="center" style={{textAlign: 'center'}}>
+                <Col span={6}>
+                    <p style={{textTransform: 'capitalize'}}>
+                        {props.data.weather[0].description}
+                    </p>
+                </Col>
 
-            <Col span={6}>
-                <img
-                    src={`http://openweathermap.org/img/wn/${props.data.weather[0].icon}.png`}
-                    alt={props.data.weather[0].description}
-                    className="weather-img"
-                />
-            </Col>
+                <Col span={6}>
+                    <img
+                        src={`http://openweathermap.org/img/wn/${props.data.weather[0].icon}.png`}
+                        alt={props.data.weather[0].description}
+                        className="weather-img"
+                    />
+                </Col>
 
-            <Col span={6}>
-                <p>{Math.round(props.data.main.temp)}° C</p>
-            </Col>
-        </Row>
+                <Col span={6}>
+                    <p>{Math.round(props.data.main.temp)}° C</p>
+                </Col>
+            </Row>
+        </div>
     );
 }
 
