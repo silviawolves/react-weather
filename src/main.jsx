f9dc213e280bc7eea058ecc0d344c54bd2b429d4
silviawolves <silviacanepa.w@gmail.com>
2022-11-22 09:36:18 +0100

    final commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import {Provider} from 'react-redux';
-import {configureStore} from '@reduxjs/toolkit';
-
 import Container from './components/Container';
-import {weatherReducer} from './store/reducers/weather.reducer';
 
 import 'antd/dist/antd.css';
 import './css/style.css';
 
-export const store = configureStore({
-    reducer: weatherReducer,
-});
-
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <Provider store={store}>
-            <Container />
-        </Provider>
+        <Container />
     </React.StrictMode>,
 );
--- src/store/reducers/weather.reducer.jsx
+++
@@ -1,6 +0,0 @@
-import {fetchWeather} from '../actions/weather.action';
-import {createReducer} from '@reduxjs/toolkit';
-
-export const weatherReducer = createReducer({}, (builder) =>
-    builder.addCase('FETCH_WEATHER', (state, action) => action.payload),
-);
